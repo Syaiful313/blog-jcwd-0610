fac974b3ece1da119f97535533ef0ca12d2e72d6
Muhammad Syaiful Mu'min <143513644+Syaiful313@users.noreply.github.com>
2025-03-04 14:38:53 +0700

    feat : add article page  and detail article (#3)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(date: string) {
+  return new Date(date).toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+}
+
+export function formatContent(content: string) {
+  return content
+    .split("\n\n")
+    .map((p) => p.trim())
+    .filter((p) => p.length > 0);
 }
