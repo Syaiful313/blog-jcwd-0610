ccff1ecfd80f0f5d9606371fc48fd5f70b0fa909
Muhammad Syaiful Mu'min <143513644+Syaiful313@users.noreply.github.com>
2025-03-09 23:42:03 +0700

    refactor : all hook (#13)

--- src/features/about/components/Team.tsx
+++ src/features/about/components/Team.tsx
@@ -3,12 +3,13 @@ import useGetAuthors from "@/hooks/api/author/useGetAuthors";
 import Image from "next/image";
 import Link from "next/link";
 import { FC } from "react";
+import TeamSkeleton from "./TeamSkeleton";
 
 const Team: FC = () => {
-  const { authors, isLoading } = useGetAuthors();
+  const { data = [], isPending } = useGetAuthors();
 
-  if (isLoading) {
-    return <p className="text-center text-gray-500">Loading authors...</p>;
+  if (isPending) {
+    return <TeamSkeleton/>
   }
 
   return (
@@ -16,35 +17,33 @@ const Team: FC = () => {
       <h2 className="mb-4 text-xl font-bold sm:mb-6 sm:text-2xl">
         Meet Our Team
       </h2>
-      {/* <Link href={`/authors/${authors}`}> */}
-        <div className="grid gap-6 sm:gap-8 md:grid-cols-2">
-          {authors.map((author, index) => (
-            <div
-              id={author.objectId}
-              key={index}
-              className="flex flex-col items-center rounded-lg border border-gray-100 p-4 text-center shadow-sm transition-shadow duration-200 hover:shadow-md sm:flex-row sm:items-start sm:space-x-4 sm:text-left dark:border-gray-800"
-            >
-              <div className="mb-3 flex-shrink-0 sm:mb-0">
-                <Image
-                  src={author.avatar}
-                  alt={author.name}
-                  width={80}
-                  height={80}
-                  className="rounded-full object-cover"
-                  priority={true}
-                />
-              </div>
-              <div>
-                <h3 className="text-lg font-bold sm:text-xl">{author.name}</h3>
-                <p className="text-muted-foreground mb-2 text-xs sm:text-sm">
-                  {author.role}
-                </p>
-                <p className="text-xs sm:text-sm">{author.bio}</p>
-              </div>
+      <div className="grid gap-6 sm:gap-8 md:grid-cols-2">
+        {data.map((author, index) => (
+          <div
+            id={author.objectId}
+            key={index}
+            className="flex flex-col items-center rounded-lg border border-gray-100 p-4 text-center shadow-sm transition-shadow duration-200 hover:shadow-md sm:flex-row sm:items-start sm:space-x-4 sm:text-left dark:border-gray-800"
+          >
+            <div className="mb-3 flex-shrink-0 sm:mb-0">
+              <Image
+                src={author.avatar}
+                alt={author.name}
+                width={80}
+                height={80}
+                className="rounded-full object-cover"
+                priority={true}
+              />
             </div>
-          ))}
-        </div>
-      {/* </Link> */}
+            <div>
+              <h3 className="text-lg font-bold sm:text-xl">{author.name}</h3>
+              <p className="text-muted-foreground mb-2 text-xs sm:text-sm">
+                {author.role}
+              </p>
+              <p className="text-xs sm:text-sm">{author.bio}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
--- src/hooks/api/author/useGetAuthors.ts
+++ src/hooks/api/author/useGetAuthors.ts
@@ -2,28 +2,16 @@
 import { Author } from "@/types/author";
 import { BASE_URL_API } from "@/utils/api";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 const useGetAuthors = () => {
-  const [authors, setAuthors] = useState<Author[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const getAuthors = async () => {
-    try {
-      const { data } = await axios.get(`${BASE_URL_API}/data/author`);
-      setAuthors(data);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    getAuthors();
-  }, []);
-
-  return { getAuthors, authors, isLoading };
+  return useQuery({
+    queryKey: ["penulis"],
+    queryFn: async () => {
+      const { data } = await axios.get<Author[]>(`${BASE_URL_API}/data/author`);
+      return data;
+    },
+  });
 };
 
 export default useGetAuthors;
