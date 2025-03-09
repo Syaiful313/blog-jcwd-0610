5ac3779fc1049f07177a4953d8214fcafc5da24d
Muhammad Syaiful Mu'min <143513644+Syaiful313@users.noreply.github.com>
2025-03-09 15:32:42 +0700

    feat : hooks author (#12)
    * feat : hooks author
    
    * fix : delete data author

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,36 +1,4 @@
-import { Author } from "@/types/author";
 import { Category } from "@/types/category";
-
-export const authors: Author[] = [
-  {
-    id: "1",
-    name: "John Doe",
-    avatar: "/johndoe.avif",
-    bio: "John is a tech enthusiast and has been writing about technology for over 10 years. He loves to explore new gadgets and share his insights with readers.",
-    role: "Editor-in-Chief",
-  },
-  {
-    id: "2",
-    name: "Jane Smith",
-    avatar: "/janesmith.avif",
-    bio: "Jane is a travel blogger who has visited over 50 countries. She shares her travel experiences, tips, and recommendations with her readers.",
-    role: "Travel Writer",
-  },
-  {
-    id: "3",
-    name: "Michael Johnson",
-    avatar: "/michael.avif",
-    bio: "Michael is a food enthusiast who loves to explore different cuisines. He shares recipes, restaurant reviews, and cooking tips on the blog.",
-    role: "Food Writer",
-  },
-  {
-    id: "4",
-    name: "Emily Davis",
-    avatar: "/emily.avif",
-    bio: "Emily is a lifestyle blogger who writes about fashion, beauty, and wellness. She shares her personal experiences and tips with her readers.",
-    role: "Lifestyle Writer",
-  },
-];
 
 export const categories: Category[] = [
   {
